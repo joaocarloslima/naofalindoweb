71e5ac4abf3b211be011487cae11b406e6aaebb4
Joao <joaocarloslima@me.com>
2023-08-25 11:51:57 -0300

    fetch

---
+++ src/app/contas/DataRow.jsx
@@ -0,0 +1,17 @@
+import DropMenu from "@/components/DropMenu";
+import { CreditCardIcon } from "@heroicons/react/24/outline";
+
+export default function DataRow({conta}){
+    return (
+        <div id="data-row" className="flex items-center justify-between hover:bg-slate-800 p-2 rounded cursor-pointer">
+            <div className="flex gap-2">
+              <CreditCardIcon className="h-6 w-6" />
+              <span>{conta.nome}</span>
+            </div>
+            <div className="flex gap-2 items-center">
+              <span>R$100.00</span>
+              <DropMenu />
+            </div>
+        </div>
+    )
+}
--- src/app/contas/page.js
+++ src/app/contas/page.js
@@ -1,13 +1,24 @@
 import NavBar from "@/components/NavBar";
+import DataRow from "./DataRow";
 
+async function getContas(){
+  const url = "http://localhost:8080/api/contas"
+  const resp = await fetch(url, { next: { revalidate: 0 } })
+  return resp.json()
+}
 
-export default function Contas() {
+export default async function Contas() {
+  const data = await getContas()
+
   return (
     <>
       <NavBar active={"contas"} />
 
       <main className="bg-slate-900 m-12 p-4 rounded">
-        <h2>Contas</h2>
+        <h2 className="text-2xl font-bold">Contas</h2>
+        <div id="data">
+          {data.map(conta => <DataRow conta={conta} />)}
+        </div>
       </main>
     </>
   )
